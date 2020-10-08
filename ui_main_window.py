1dcf40d98fde1ce8bf7525844f55c1416def01e8
Bitum Hoang <bitumhoang@MacBook-Pro-cua-Bitum.local>
2020-10-08 23:34:54 +0700

    #002_label_slider

--- ui_main_window.py
+++ ui_main_window.py
@@ -7,6 +7,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -23,6 +25,37 @@ class Ui_Form(object):
         self.control_bt.setObjectName("control_bt")
         self.verticalLayout.addWidget(self.control_bt)
         self.horizontalLayout.addLayout(self.verticalLayout)
+        #set slider
+
+        
+        self.sliderLayout = QtWidgets.QVBoxLayout()
+        self.sliderLayout.setObjectName("sliderLayout")
+        self.slider = QtWidgets.QSlider()
+        self.slider.setOrientation(Qt.Horizontal)
+        self.slider.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.slider.setTickInterval(1)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.valueChanged.connect(self.changedValue)
+        self.slider.setObjectName("slider_bar")
+        self.label = QtWidgets.QLabel("0")
+        self.label.setFont(QtGui.QFont("Sanserif", 15))
+        self.sliderLayout.addWidget(self.label)
+        self.sliderLayout.addWidget(self.slider)
+        self.horizontalLayout.addLayout(self.sliderLayout)
+
+        # #slider
+        # self.slider = QSlider()
+        # self.slider.setOrientation(Qt.Horizontal)
+        # self.slider.setTickPosition(QSlider.TicksBelow)
+        # self.slider.setTickInterval(1)
+        # self.slider.setMinimum(0)
+        # self.slider.setMaximum(100)
+        # self.slider.valueChanged.connect(self.changedValue)
+        # self.label = QLabel("0")
+        # self.label.setFont(QtGui.QFont("Sanserif", 15))
+        # self.verticalLayout.addWidget(self.slider)
+        # self.verticalLayout.addWidget(self.label)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -32,3 +65,9 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "Cam view"))
         self.image_label.setText(_translate("Form", "TextLabel"))
         self.control_bt.setText(_translate("Form", "Start"))
+
+
+    def changedValue(self):
+        size = self.slider.value()
+        self.label.setText(str(size))
+        
